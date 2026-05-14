24ad1655001dc9ffbfc6c68eb3d3444389b7ec24
mhd-faizzan <muhammadfaizzan@outlook.com>
2026-05-14 12:10:06 +0200

    add auth0 provider setup

---
+++ frontend/src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import { Auth0Provider } from "@auth0/auth0-react"
+import App from "./App.jsx"
+import "./index.css"
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+        audience: "http://localhost:8000"
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </StrictMode>
+)
